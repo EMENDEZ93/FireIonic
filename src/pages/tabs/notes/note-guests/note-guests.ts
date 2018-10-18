9e0698d501bbc9fd15c9f0a527b0be6a6bc6d5db
- Edwin Mendez - <emendez@unac.edu.co>
2018-10-17 22:57:02 -0500

    guest.status

--- src/pages/tabs/notes/note-guests/note-guests.ts
+++ src/pages/tabs/notes/note-guests/note-guests.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {NoteProvider} from "../../../../services/note/note.service";
 import firebase from "firebase";
-import {InvitationService} from "../../../../services/tabs/notes/invitation.service";
 
 @IonicPage()
 @Component({
@@ -13,8 +12,7 @@ export class NoteGuestsPage {
 
   note_id;
   guests;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public noteProvider: NoteProvider,
-              public invitationService: InvitationService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public noteProvider: NoteProvider) {
     this.note_id = this.navParams.get("note_id");
   }
 
@@ -24,13 +22,9 @@ export class NoteGuestsPage {
 
   ionViewWillEnter(){
     this.noteProvider.getAllGuestsByNoteIdAndHost(this.note_id, firebase.auth().currentUser.email).subscribe(
-      (guests) =>{ this.guests = guests; console.log(guests)},
+      (guests) =>{ this.guests = guests;},
       (error) => {console.log(error)}
     );
-  }
-
-  getInvitationStatusByNoteIdAndGuest(guest_email){
-    this.invitationService.getInvitationStatusByNoteIdAndGuest(this.note_id, guest_email);
   }
 
 }
--- src/services/tabs/notes/invitation.service.ts
+++ src/services/tabs/notes/invitation.service.ts
@@ -7,9 +7,4 @@ export class InvitationService {
   constructor(public httpClient: HttpClient){
   }
 
-  getInvitationStatusByNoteIdAndGuest(note_id, guest_email){
-    return this.httpClient
-      .get("https://fire-backend.herokuapp.com/get/invitation/status/by/noteid/" + note_id + "/and/guest/" + guest_email );
-  }
-
 }
